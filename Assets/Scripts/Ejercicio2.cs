3ecb700e75b7074857f5f8549baab1180f9ec1dd
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-02-10 14:19:23 +0100

    Initial commit

---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Ejercicio2 : MonoBehaviour {
+	
+	public Rect rectEnunciado;
+	Rect rectEjercicio;
+
+
+	public int xEnunciado;
+	private int yEnunciado = 35;
+	public int widthEnunciado = 500;
+	public int heightEnunciado = 100;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update() {
+		xEnunciado = (Screen.width);
+	}
+	void OnGUI(){
+		rectEnunciado = new Rect(xEnunciado,yEnunciado, widthEnunciado, heightEnunciado);
+		GUI.Label (rectEnunciado, "Pulsa espacio para mostrar el otro mensaje");
+	}
+}
---
+++ Assets/Scripts/Salir.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Salir : MonoBehaviour {
+
+	private string escenaMenu = "main";
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && !SceneManager.GetActiveScene().name.Equals(escenaMenu)) {
+			GoMenu ();
+		}
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+
+	public void GoMenu() {
+		SceneManager.LoadScene (escenaMenu);
+	}
+}
